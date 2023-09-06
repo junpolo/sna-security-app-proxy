232a6f25e21357a1c6fd2b26ba4bf8b1059a3e0b
Jun Polo <junpolojr@gmail.com>
2023-09-06 10:18:26 +0800

    refactor: create custom post auth

--- securityHttpTrigger/index.ts
+++ securityHttpTrigger/index.ts
@@ -1,17 +1,54 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import * as https from "https";
+import * as querystring from "querystring";
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  const body = req.body;
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+  return new Promise((resolve, reject) => {
+    if (!body) {
+      context.res = {
+        status: 400,
+        body: "Please pass a request",
+      };
+
+      reject();
+    }
+
+    const facebookClientSecret = "2925820d6bea115e74be1bb58309985c";
+    const options = {
+      host: "graph.facebook.com",
+      port: 443,
+      path: "/v3.1/oauth/access_token",
+      method: "POST",
     };
+    const params = querystring.parse(body);
+    params.client_secret = facebookClientSecret;
+    const postData = querystring.stringify();
 
+    let result = null;
+
+    const request = https.request(options, (response) => {
+      response.on("data", (chunk) => {
+        result = chunk;
+      });
+
+      response.on("end", () => {
+        context.res = {
+          status: 200,
+          body: result,
+        };
+      });
+    });
+
+    request.write(postData);
+    request.end();
+
+    resolve();
+  });
 };
 
 export default httpTrigger;
